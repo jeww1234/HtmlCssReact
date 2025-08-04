a92f56d243c9ea0fcc5e210ab3cf9d247300cebe
JJH <jeww1234@gmail.com>
2025-08-05 03:07:47 +0900

    e

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
 import { Route, Routes} from 'react-router';
-import Google from './component/Google'
-import Home from './component/Home'
-import Flex from './component/Flex'
-import Spotify from './component/Spotify';
+import Home from './component/HomePage/Home'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/google' element={<Google />}></Route>
-        <Route path='/' element={<Home />}> </Route>
-        <Route path='/flex' element={<Flex />}></Route>
-        <Route path='/spotify' element={<Spotify />}></Route>
+        <Route path='/' element={<Home />}></Route>
       </Routes>     
     </div>
   );
